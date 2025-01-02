0797da09967626ee576bfce23538046ebe4fe068
Hexin <hexin.email@qq.com>
2025-01-02 12:48:44 +0800

    登录界面设计

--- ReactorControlSystem/Modules/ReactorControlSystem.Modules.Login/ViewModels/LoginViewModel.cs
+++ ReactorControlSystem/Modules/ReactorControlSystem.Modules.Login/ViewModels/LoginViewModel.cs
@@ -1,15 +1,23 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
+﻿using Prism.Mvvm;
 using Prism.Services.Dialogs;
+using ReactorControlSystem.Core.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Windows;
 
 namespace ReactorControlSystem.Modules.Login.ViewModels
 {
     public class LoginViewModel : BindableBase, IDialogAware
     {
         public string Title => "账号登录";
+
+        public string Version => Application.ResourceAssembly.GetName().Version.ToString();
+
+        private LoginModel _loginModel = new LoginModel();
+        public LoginModel LoginModel
+        {
+            get { return _loginModel; }
+            set { SetProperty(ref _loginModel, value); }
+        }
 
         public event Action<IDialogResult> RequestClose;
 
---
+++ ReactorControlSystem/ReactorControlSystem.Core/Models/LoginModel.cs
@@ -0,0 +1,38 @@
+﻿using ReactorControlSystem.Core.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReactorControlSystem.Core.Models
+{
+    public class LoginModel : NotifyDataErrorInfo
+    {
+        private string _username = string.Empty;
+
+        [Required(ErrorMessage = "用户名不能为空")]
+        public string Username
+        {
+            get { return _username; }
+            set 
+            {
+                if (SetProperty(ref _username, value))
+                {
+                    Validate(nameof(Username), value);
+                }
+            }
+        }
+
+        private string _password = string.Empty;
+
+        [Required(ErrorMessage = "密码不能为空")]
+        public string Password
+        {
+            get { return _password; }
+            set 
+            { 
+                if (SetProperty(ref _password, value))
+                {
+                    Validate(nameof(Password), value);
+                }
+            }
+        }
+    }
+}
