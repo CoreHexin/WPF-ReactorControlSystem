8af873cf906fece1e78c7098a83a382bd00083b9
Hexin <hexin.email@qq.com>
2025-01-10 18:48:13 +0800

    添加系统配置模块

---
+++ ReactorControlSystem/Modules/ReactorControlSystem.Modules.Settings/SettingsModule.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using ReactorControlSystem.Modules.Settings.ViewModels;
+using ReactorControlSystem.Modules.Settings.Views;
+
+namespace ReactorControlSystem.Modules.Settings
+{
+    public class SettingsModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<SettingsView, SettingsViewModel>();
+        }
+    }
+}
---
+++ ReactorControlSystem/Modules/ReactorControlSystem.Modules.Settings/Views/SettingsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ReactorControlSystem.Modules.Settings.Views
+{
+    /// <summary>
+    /// Interaction logic for SettingsView
+    /// </summary>
+    public partial class SettingsView : UserControl
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
