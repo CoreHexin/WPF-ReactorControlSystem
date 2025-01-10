c5a4807c13d770f8e8cd061cd840c9bd9297bdff
Hexin <hexin.email@qq.com>
2025-01-10 18:47:55 +0800

    添加首页模块

---
+++ ReactorControlSystem/Modules/ReactorControlSystem.Modules.Home/HomeModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using ReactorControlSystem.Modules.Home.Views;
+
+namespace ReactorControlSystem.Modules.Home
+{
+    public class HomeModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
